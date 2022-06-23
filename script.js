1dcd58dde887511470bc7efa65111c653efb7a2e
Yasmin vitória costa <99223801+YASMINCOS@users.noreply.github.com>
2022-06-22 22:21:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const toastButton= document.getElementById('button');
+const toastMessage= document.getElementById('toast');
+
+toastButton.addEventListener('click', showToast);
+
+function showToast(){
+    toastMessage.classList.add('show-toast');
+}
