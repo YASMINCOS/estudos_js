d5773833f8a048c7a39cec65786c46b8ffb82d93
Yasmin vitória costa <99223801+YASMINCOS@users.noreply.github.com>
2022-06-24 22:00:00 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const toastButton= document.getElementById('button');
-const toastMessage= document.getElementById('toast');
-
-toastButton.addEventListener('click', showToast);
-
-function showToast(){
-    toastMessage.classList.add('show-toast');
-}
